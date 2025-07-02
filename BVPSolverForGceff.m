ded81e96c2b80a204a02a579e1c0e60aa6c8d4b0
charvey2718 <59702518+charvey2718@users.noreply.github.com>
2025-07-02 20:18:38 +0100

    Add files via upload

--- BVPSolverForGceff.m
+++ BVPSolverForGceff.m
@@ -5,10 +5,10 @@ clc
 global ls h b Gcint Gcbulk Gceff E nu alpha1 alpha2
 E = 126e9; % Young's modulus (Pa)
 nu = 0.3; % Poisson's ratio
-ls = 80e-6; % length scale (m)
+ls = 20e-6; % length scale (m)
 b = 0.05e-3; % half interface thickness (m)
 Gcint = 281; % interface fracture toughness (N/m)
-Gcbulk = 5*Gcint; % bulk fracture toughness (N/m)
+Gcbulk = 10*Gcint; % bulk fracture toughness (N/m)
 h = b/12; % interface element size (m)
 Gctol = 0.01; % Iterate until Gceff converges to within this tolerance
 warning('off', 'all');
@@ -20,17 +20,16 @@ alpha2 =(2*Gceff*exp((b - h/2)/ls))/(Gcbulk*exp((b - h/2)/ls) - Gcbulk*exp(-(b -
 fprintf('Yoshioka et al. (2021):\t%.2f\n', Gceff);
 
 %% Method 1
-[x, s, S] = iterativelySolve(@f1, @bc, @guess_left, @guess_right, b, h, ls, Gcint, Gcbulk, Gctol);
+[x_method1, s_method1, S_method1] = iterativelySolve(@f1, @bc, @guess_left, @guess_right, b, h, ls, Gcint, Gcbulk, Gctol);
 fprintf('Method 1:\t%.2f\n', Gceff)
-
 %% Method 2
-[x, s, S] = iterativelySolve(@f2, @bc, @guess_left, @guess_right, b, h, ls, Gcint, Gcbulk, Gctol);
+[x_method2, s_method2, S_method2] = iterativelySolve(@f2, @bc, @guess_left, @guess_right, b, h, ls, Gcint, Gcbulk, Gctol);
 fprintf('Method 2:\t%.2f\n', Gceff)
-
 %% Method 3
-[x, s, S] = iterativelySolve(@f3, @bc, @guess_left, @guess_right, b, h, ls, Gcint, Gcbulk, Gctol);
+[x_method3, s_method3, S_method3] = iterativelySolve(@f3, @bc, @guess_left, @guess_right, b, h, ls, Gcint, Gcbulk, Gctol);
 fprintf('Method 3:\t%.2f\n', Gceff)
 
+%% Functions
 function [x, s, S] = iterativelySolve(f, bc, guess_left, guess_right, b, h, ls, Gcint, Gcbulk, Gctol)
     % The function f depends on Gceff, which is also being solved for. This
     % function therefore solves f iteratively, updating Gceff each time,
@@ -38,10 +37,10 @@ function [x, s, S] = iterativelySolve(f, bc, guess_left, guess_right, b, h, ls, 
 
     global Gceff
 
-    xmesh_left = [linspace(0, b, 20000), linspace(b, 20*b, 20000)]; % starting from 0
+    xmesh_left = [linspace(0, b, 20000), linspace(b, 40*b, 20000)]; % starting from 0
     solinit_left = bvpinit(xmesh_left, guess_left);
     
-    xmesh_right = [linspace(h/2, b, 20000), linspace(b, 20*b, 20000)];
+    xmesh_right = [linspace(h/2, b, 20000), linspace(b, 40*b, 20000)];
     solinit_right = bvpinit(xmesh_right, guess_right);
 
     options = bvpset('RelTol', 1e-9, 'AbsTol', 1e-9);
@@ -50,7 +49,7 @@ function [x, s, S] = iterativelySolve(f, bc, guess_left, guess_right, b, h, ls, 
         sol_right = bvp4c(f, bc, solinit_right, options);
         x = [-flipud(sol_left.x'); sol_right.x'];
         s = [flipud(sol_left.y(1,:)'); sol_right.y(1,:)'];
-        DeltaGceff = Gceff_from_sProfile(x, s, b, ls, h, Gcint, Gcbulk) - Gceff; % change in Gceff
+        DeltaGceff = Gceff_from_sProfile(x, s, b, ls, Gcint, Gcbulk) - Gceff; % change in Gceff
         Gceff = Gceff + DeltaGceff; % update Gceff
         if abs(DeltaGceff) < Gctol % is Gceff within tolerance?
             break;
@@ -92,9 +91,9 @@ function dsdx = f3(x, s, region)
     % Calculate k from sharp crack theory
     switch region
         case 1
-            k = (11 - 5*nu)/(16*ls*pi*x*(s(1) + 1e-12)^2)*Gcint/Gceff;
+            k = (11 - 5*nu)/(16*ls*pi*x*s(1))*Gcint/Gceff;
         case 2
-            k = (11 - 5*nu)/(16*ls*pi*x*s(1)^2)*Gcint/Gcbulk;
+            k = (11 - 5*nu)/(16*ls*pi*x*s(1))*Gcint/Gcbulk;
     end
 
     % Limit k to kmax
@@ -138,7 +137,7 @@ function g = guess_right(x, ~)
     % Use Yoshioka et al.'s (2021) s profile with the element-size effect
     % as the initial guess for the right side.
 
-    global alpha1 alpha2 h b ls
+    global alpha1 alpha2 h ls
     if abs(x) < h/2
         g = [0; 0];
     elseif abs(x) > h/2
@@ -150,7 +149,7 @@ function g = guess_right(x, ~)
     end
 end
 
-function Gceff = Gceff_from_sProfile(x, s, b, ls, h, Gcint, Gcbulk)
+function Gceff = Gceff_from_sProfile(x, s, b, ls, Gcint, Gcbulk)
     % Apply the energy balance equation to compute Gceff.
 
     B = unique([x s],'rows');
@@ -163,5 +162,5 @@ function Gceff = Gceff_from_sProfile(x, s, b, ls, h, Gcint, Gcbulk)
     ds_dx = gradient(s)./gradient(x);
     S = 1/2*((1 - s).^2/ls + ls*ds_dx.^2);
 
-    Gceff = (Gcint/2 + Gcint*(h/4/ls + 1/2) - Gcbulk/2*trapz(x(bulk_left), S(bulk_left)) - Gcbulk/2*trapz(x(bulk_right), S(bulk_right)))/trapz(x(intf), S(intf));
+    Gceff = (Gcint - Gcbulk*trapz(x(bulk_left), S(bulk_left)) - Gcbulk*trapz(x(bulk_right), S(bulk_right)))/trapz(x(intf), S(intf));
 end
